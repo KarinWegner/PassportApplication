99bbb0f904e4b94da561a4dc203180a9c3ad1db0
Karin Wegner <karinwegner00@gmail.com>
2024-11-14 13:44:48 +0100

    Added users dbSet

---
+++ PassportApplication/Data/Migrations/20241114121211_addedDbSet.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassportApplication.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedDbSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApplicationUserGymClass_AspNetUsers_ApplicationUserId",
+                table: "ApplicationUserGymClass");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApplicationUserGymClass_GymClasses_GymClassId",
+                table: "ApplicationUserGymClass");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ApplicationUserGymClass",
+                table: "ApplicationUserGymClass");
+
+            migrationBuilder.RenameTable(
+                name: "ApplicationUserGymClass",
+                newName: "Bookings");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ApplicationUserGymClass_GymClassId",
+                table: "Bookings",
+                newName: "IX_Bookings_GymClassId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ApplicationUserGymClass_ApplicationUserId",
+                table: "Bookings",
+                newName: "IX_Bookings_ApplicationUserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationUserId",
+                table: "Bookings",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Bookings",
+                table: "Bookings",
+                columns: new[] { "UserId", "ClassId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_AspNetUsers_ApplicationUserId",
+                table: "Bookings",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_GymClasses_GymClassId",
+                table: "Bookings",
+                column: "GymClassId",
+                principalTable: "GymClasses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_AspNetUsers_ApplicationUserId",
+                table: "Bookings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_GymClasses_GymClassId",
+                table: "Bookings");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Bookings",
+                table: "Bookings");
+
+            migrationBuilder.RenameTable(
+                name: "Bookings",
+                newName: "ApplicationUserGymClass");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Bookings_GymClassId",
+                table: "ApplicationUserGymClass",
+                newName: "IX_ApplicationUserGymClass_GymClassId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Bookings_ApplicationUserId",
+                table: "ApplicationUserGymClass",
+                newName: "IX_ApplicationUserGymClass_ApplicationUserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationUserId",
+                table: "ApplicationUserGymClass",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ApplicationUserGymClass",
+                table: "ApplicationUserGymClass",
+                columns: new[] { "UserId", "ClassId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApplicationUserGymClass_AspNetUsers_ApplicationUserId",
+                table: "ApplicationUserGymClass",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApplicationUserGymClass_GymClasses_GymClassId",
+                table: "ApplicationUserGymClass",
+                column: "GymClassId",
+                principalTable: "GymClasses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
